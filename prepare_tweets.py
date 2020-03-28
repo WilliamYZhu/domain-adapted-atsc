41d48830cf2a3e675cee6301bf3392606a175c1e
Louis Qin <qyl0509@icloud.com>
2020-03-28 15:55:19 -0700

    prepare tweets

--- prepare_tweets.py
+++ prepare_tweets.py
@@ -1,5 +1,9 @@
 import argparse
 import os
+import json
+import re
+from tqdm import tqdm
+import spacy
 
 RAW_DIR = "./data/raw/tweets"
 TRANS_DIR = "./data/transformed/tweets"
@@ -21,6 +25,15 @@ def scrape():
     get_tweets("coronavirus")
     get_tweets("CDC")
 
+def sentence_segment_filter_docs(doc_array):
+    # get sentence segemented review with #sentences > 2
+    sentences = []
+    
+    for doc in nlp.pipe(doc_array, disable=['parser', 'tagger', 'ner'], batch_size=1000, n_threads=8):
+        sentences.append([sent.text.strip() for sent in doc.sents])
+
+    return sentences
+
 def main():
     parser = argparse.ArgumentParser(description='Generate finetuning corpus for tweets.')
     parser.add_argument('--scrape',
@@ -36,4 +49,27 @@ def main():
         if filename.endswith(".json"):
             with open(f"{RAW_DIR}/{filename}") as file:
                 for line in file:
-                    print(json.loads(line)['tweet'])
+                    tweet = json.loads(line)['tweet']
+                    no_url = re.sub(r'https?:\/\/.*[\r\n]*', '', tweet, flags=re.MULTILINE)
+                    tweets.append(no_url)
+    
+    print(f'Found {len(tweets)} tweets')
+    print(f'Tokenizing tweets...')
+
+    sentences = sentence_segment_filter_docs(tweets)
+    nr_sents = sum([len(s) for s in sentences])
+    print(f'Segmented {nr_sents} tweet sentences')
+
+    with open(f"{TRANS_DIR}/tweet_corpus.txt", "w") as f:
+        for sents in tqdm(sentences):
+            real_sents = []
+            for s in sents:
+                x = s.replace(' ', '').replace('\n', '').replace('\u200d', '').replace('\u200b', '')
+                if x != '':
+                   real_sents.append(s.replace('\n', '').replace('\u200d', '').replace('\u200b', ''))
+            if len(real_sents) >= 2:
+                str_to_write = "\n" + "\n".join(real_sents) + "\n"
+                f.write(str_to_write)
+
+if __name__ == "__main__":
+    main()
